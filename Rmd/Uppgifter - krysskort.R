06bf35b47c5fa5c09195d9909f67a494f34ee5c7
adamflr <flr.adam@gmail.com>
2022-07-28 11:39:41 +0200

    Correct CE2

--- Rmd/Uppgifter - krysskort.R
+++ Rmd/Uppgifter - krysskort.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 tab <- expand_grid(Question = 1:30 + rep(1:6, each = 5),
-                   Exercise = seq(1, 4, length.out = 7)) %>% 
+                   Exercise = seq(1, 4, length.out = 8)) %>% 
   group_by(Exercise) %>% 
   mutate(Rank = rank(Question),
          Type = ifelse(Rank %in% 1:20, "Sal", 
@@ -9,7 +9,7 @@ tab <- expand_grid(Question = 1:30 + rep(1:6, each = 5),
 ggplot(tab, aes(Question, Exercise, color = Type)) +
   geom_point(shape = 21, size = 10) +
   # geom_vline(xintercept = seq(5.5, 30.5, 5)) +
-  annotate("text", x = 0, y = rev(unique(tab$Exercise)), label = paste("Datorövning", 1:7), hjust = 1, family = "serif") +
+  annotate("text", x = 0, y = rev(unique(tab$Exercise)), label = paste("Datorövning", 1:length(unique(tab$Exercise))), hjust = 1, family = "serif") +
   annotate("text", x = c(25.5, 31.5), y = max(tab$Exercise) + 0.5, label = c("Bonus", "Hem"), family = "serif", hjust = 0) +
   cowplot::theme_nothing() +
   xlim(-4,43)
