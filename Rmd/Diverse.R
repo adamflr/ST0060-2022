fef3ae3ec2ecb3d8075add08af477c49288af53a
adamflr <flr.adam@gmail.com>
2022-08-09 14:09:58 +0200

    CE 6 and CE 7 knitted

--- Rmd/Diverse.R
+++ Rmd/Diverse.R
@@ -1,6 +1,9 @@
-a <- read_lines("Rmd/Datorövning-4.Rmd")
-sum(grepl("exercise", a))
+tibble(Datorövning = 1:7,
+       file = paste0("Rmd/Datorövning-", Datorövning, ".Rmd")) %>% 
+  mutate(text = map(file, read_lines)) %>% 
+  mutate(Exercises = map_dbl(text, ~ sum(grepl("exercise", .x))))
 
+# Allsvenskan. Poisson ----
 library(tidyverse)
 dat <- read_csv("https://raw.githubusercontent.com/adamflr/ST0060-2022/main/Data/Allsvenskan%2C%20herrar%2C%201924-2020.csv")
 
@@ -31,6 +34,49 @@ dat %>%
   mutate(p = n / sum(n))
 
 dat_ore %>% 
-  select(-stat_id) %>% 
-  pivot_longer(-(1:2), names_to = "Vehicle", values_to = "Count") %>% 
+  mutate(Time = paste0(Year, "-", ifelse(Month<10, paste0("0", Month), Month), "-", "01")) %>% 
+  select(1,2,5,3,4) %>% 
   write_csv("Oresund-bridge-traffic.csv")
+
+ggplot() + geom_histogram(aes(x = rbinom(10000, 100, 0.9)))
+
+dat %>% 
+  filter(as.numeric(Säsong) > 1990) %>% 
+  count(Bortamål) %>% 
+  mutate(p = n / sum(n),
+         pois = dpois(Bortamål, lambda = sum(p * Bortamål))) %>% 
+  mutate(E = sum(n) * pois,
+         chisq = (n - E)^2 / E) %>% 
+  summarise(E = sum(E) * 100, n = sum(n), chisq = sum(chisq))
+1 - pchisq(17.5, df = 7)
+
+# Feromoninventering. Leaflet ----
+library(readxl)
+dat_leaf <- read_excel("Data/Artportalen, SLU feromoninventering, 2011-2013.xlsx", skip = 2)
+
+m <- leaflet() %>%
+  addTiles() %>%
+  addMarkers(lng = dat_leaf$Nordkoordinat, lat = dat_leaf$Ostkoordinat)
+m
+
+library(sf)
+
+coord <- dat_leaf %>% 
+  mutate(Ostkoordinat = Ostkoordinat + 50 * (runif(n()) - 0.5),
+         Nordkoordinat = Nordkoordinat + 50 * (runif(n()) - 0.5)) %>% 
+  st_as_sf(coords = c("Ostkoordinat", "Nordkoordinat")) %>% 
+  `st_crs<-`(3847) %>% 
+  st_transform("+proj=longlat") %>% 
+  st_coordinates() %>% 
+  as_tibble()
+
+m <- leaflet() %>%
+  addTiles() %>%
+  addCircleMarkers(lng = coord$X, lat = coord$Y, popup = "TEXT")
+m
+
+coord2 <- coord %>% 
+  rename(lng = X, lat = Y)
+dat_leaf <- bind_cols(dat_leaf, coord2)
+
+openxlsx::write.xlsx(dat_leaf, "Data/Artportalen, feromoninventering, 2011-2013.xlsx")
